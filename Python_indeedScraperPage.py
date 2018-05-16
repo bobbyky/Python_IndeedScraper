edade0cbd70ab9c7d3d46154c0f81d6e7b1e905d
bobbyky <39269977+bobbyky@users.noreply.github.com>
2018-05-16 17:11:35 +1000

    Add files via upload

---
+++ Python_indeedScraperPage.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+from flask import Flask, abort, request
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import requests
+import json
+
+
+app = Flask(__name__)
+
+urls = []
+
+@app.route('/', methods=['GET','POST'])
+def foo():
+    if not request.json:
+        abort(400)
+    info = request.json
+
+    for a in range(len(info['jobs'])):
+        url = (info['jobs'][a])
+        urls.append(url)
+
+        # Retrieve contents of job list page from indeed.com
+        jobs = requests.get(urls[a])
+        # URL linking to indeed.com page containing
+        # list of jobs
+
+        # list used to store URLs linking to job postings
+        job_urls = []
+
+        # Parse through the page using Python's built-in
+        # HTML parser
+        job_soup = BeautifulSoup(jobs.text, "html.parser")
+        # Find the tags that correspond to
+        # each job listings' url,
+        # store the url of the job in job_url and append it
+        # to the list of job urls
+        job_tbl = job_soup.find(id = "resultsCol")
+
+        for link in job_tbl.find_all("a", attrs = {"class" : "turnstileLink"}):
+            job_url = urljoin(urls[a], link.attrs["href"])
+            job_urls.append(job_url)
+
+        list =[]
+        # Iterate through each url in job_urls
+        # For each job listing, print its title, location, company, and url
+        for x in range(len(job_urls)):
+            info = {}
+
+            # Content of web page retrieved using get() function
+            # Stored in variable job_page
+            job_page = requests.get(job_urls[x])
+
+            # Parse through the page using Python's built-in
+            # HTML parser
+            soup = BeautifulSoup(job_page.text, "html.parser")
+
+            # Find the tags that correspond to
+            # the job posting's title, location, company, and url.
+            # Print each job posting's title, location, company, and url.
+            job_title = soup.find(class_='jobtitle').get_text()
+
+            info['title'] = job_title
+
+            job_location = soup.find(class_='location').get_text()
+            info['location'] = job_location
+
+            job_company = soup.find(class_='company').get_text()
+            info['company'] = job_company
+
+            info['url'] = job_urls[x]
+
+            list.append(info)
+    return json.dumps(list, indent=4)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
