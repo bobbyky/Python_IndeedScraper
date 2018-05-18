02b37aa5dae395649a8eccf7195a7593af9884bf
bobbyky <39269977+bobbyky@users.noreply.github.com>
2018-05-18 21:01:47 +1000

    Add files via upload

--- Python_indeedScraperPage.py
+++ Python_indeedScraperPage.py
@@ -6,18 +6,24 @@ from urllib.parse import urljoin
 import requests
 import json
 
-
+# create an instance of a web server
 app = Flask(__name__)
 
+# List used to store the list of urls passed through curl command
 urls = []
 
 @app.route('/', methods=['GET','POST'])
-def foo():
+def find_job_info():
     if not request.json:
         abort(400)
-    info = request.json
 
+    # info used to store information from -d (data) field of curl command
+    info = request.json
+    # list used to store each job listing's url
+    url_list = []
     for a in range(len(info['jobs'])):
+
+        # iterates through the curl command to find each url associated with the 'jobs' name
         url = (info['jobs'][a])
         urls.append(url)
 
@@ -38,42 +44,48 @@ def foo():
         # to the list of job urls
         job_tbl = job_soup.find(id = "resultsCol")
 
-        for link in job_tbl.find_all("a", attrs = {"class" : "turnstileLink"}):
+        # iterates through job_tbl to find all elements with the corresponding
+        # name and attribute, job_url is the url of each individual job listing
+        for link in job_tbl.find_all('a', attrs = {'data-tn-element' : 'jobTitle'}):
             job_url = urljoin(urls[a], link.attrs["href"])
             job_urls.append(job_url)
+        url_list.extend(job_urls)
 
-        list =[]
-        # Iterate through each url in job_urls
-        # For each job listing, print its title, location, company, and url
-        for x in range(len(job_urls)):
-            info = {}
+    # list used to store the information scraped from each job listing
+    info_list = []
 
-            # Content of web page retrieved using get() function
-            # Stored in variable job_page
-            job_page = requests.get(job_urls[x])
+    # Iterate through each url in job_urls
+    # For each job listing, print its title, location, company, and url
+    for x in range(len(url_list)):
+        info = {}
+        # Content of web page retrieved using get() function
+        # Stored in variable job_page
+        job_page = requests.get(url_list[x])
 
-            # Parse through the page using Python's built-in
-            # HTML parser
-            soup = BeautifulSoup(job_page.text, "html.parser")
+        # Parse through the page using Python's built-in
+        # HTML parser
+        soup = BeautifulSoup(job_page.text, "html.parser")
 
-            # Find the tags that correspond to
-            # the job posting's title, location, company, and url.
-            # Print each job posting's title, location, company, and url.
-            job_title = soup.find(class_='jobtitle').get_text()
+        # Find the tags that correspond to
+        # the job posting's title, location, company, and url.
+        # Print each job posting's title, location, company, and url.
+        job_title = soup.find(class_='jobtitle').get_text()
 
-            info['title'] = job_title
+        info['title'] = job_title
 
-            job_location = soup.find(class_='location').get_text()
-            info['location'] = job_location
+        job_location = soup.find(class_='location').get_text()
+        info['location'] = job_location
 
-            job_company = soup.find(class_='company').get_text()
-            info['company'] = job_company
+        job_company = soup.find(class_='company').get_text()
+        info['company'] = job_company
 
-            info['url'] = job_urls[x]
+        info['url'] = url_list[x]
 
-            list.append(info)
-    return json.dumps(list, indent=4)
+        info_list.append(info)
 
+    return json.dumps(info_list, indent=4)
+
+# start the web server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
--- Python_indeedScraperURL.py
+++ Python_indeedScraperURL.py
@@ -5,21 +5,27 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+# create an instance of a web server
 app = Flask(__name__)
 
+# List used to store the list of urls passed through curl command
 urls = []
 
 @app.route('/', methods=['GET','POST'])
-def foo():
+def find_job_info():
     if not request.json:
         abort(400)
+    # info used to store information from -d (data) field of curl command
     info = request.json
 
+    # iterates through the curl command to find each url associated with the 'jobs' name
     for a in range(len(info['jobs'])):
         url = (info['jobs'][a])
         urls.append(url)
 
-    list = []
+    # list used to store the information scraped from each job listing
+    info_list = []
+
     # Iterates through each url in the list
     # For each job listing, print its title, location, company, and url
     for x in range(len(urls)):
@@ -45,10 +51,10 @@ def foo():
         info['company'] = job_company
 
         info['url'] = urls[x]
-        list.append(info)
+        info_list.append(info)
 
-    return json.dumps(list, indent = 4)
+    return json.dumps(info_list, indent = 4)
 
-
+# start the web server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
